20227265f7c5277a15454b2dc2e5b60ea7ed9fc7
ksptooi <ksptooi@protonmail.ch>
2021-03-17 00:10:26 +0800

    N7快速排序

---
+++ n7-fast-sort/src/main/java/com/ksptooi/n7/FastSort.java
@@ -0,0 +1,32 @@
+package com.ksptooi.n7;
+
+/**
+ * N7项目 - 快速排序
+ */
+public class FastSort {
+
+
+    public static void main(String[] args) {
+
+        int[] resource = {2,432,42,321,2,56,0,543,4,54,543,987,16,154,12,65,645,954,21,243};
+
+        int benchmarkValue = resource[1];
+        int left = 0;
+        int right = resource.length;
+
+        if(left>right){
+            return;
+        }
+
+        for(;left<right;){
+
+            
+
+        }
+
+
+
+    }
+
+
+}
